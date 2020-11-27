4d6aa0960fbfb763086b7471042ee9c7934657df
renatojaraor <57976123+renatojaraor@users.noreply.github.com>
2020-11-27 12:28:16 -0500

    commit inicial

---
+++ app/src/main/java/com/example/megacine/P_Acierto.java
@@ -0,0 +1,14 @@
+package com.example.megacine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P_Acierto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p__acierto);
+    }
+}
