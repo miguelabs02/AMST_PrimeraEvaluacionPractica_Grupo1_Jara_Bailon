81ab0a138579d012d1e60820c8aebc5b9e324844
renatojaraor <57976123+renatojaraor@users.noreply.github.com>
2020-11-27 17:15:53 -0500

    final

--- app/src/main/java/com/example/megacine/P_Fallo.java
+++ app/src/main/java/com/example/megacine/P_Fallo.java
@@ -2,13 +2,35 @@ package com.example.megacine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class P_Fallo extends AppCompatActivity {
-
+    TextView tv;
+    int ac;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p__fallo);
+        Bundle bun = this.getIntent().getExtras();
+        ac = (Integer) bun.getSerializable("aciertos");
+
+        tv = (TextView) findViewById(R.id.textView4);
+
+        tv.setText("Numero de aciertos: "+ac);
+    }
+
+    public void volverJugar(View view){
+        Intent inicio = new Intent(P_Fallo.this, MainActivity.class);
+        startActivity(inicio);
+    }
+
+    public void salir(View view){
+        Intent menu = new Intent(P_Fallo.this, MainActivity.class);
+        startActivity(menu);
     }
 }
--- app/src/main/java/com/example/megacine/Preguntas.java
+++ app/src/main/java/com/example/megacine/Preguntas.java
@@ -21,18 +21,12 @@ public class Preguntas extends AppCompatActivity {
     Button opcion_2;
     Button opcion_3;
     Button opcion_4;
-
     //ArrayList<Integer> preguntas_expuestas;
-    Boolean Ganador;
-    Boolean respuesta_correcta;
-    int aciertos;
+    //Boolean Ganador;
+    //Boolean respuesta_correcta;
+    public int aciertos;
     ArrayList<Integer> random;
     ArrayList<Pregunta> p;
-    /*ImageView im1;
-    ImageView im2;
-    ImageView im3;
-    ImageView im4;
-    ImageView im5;*/
 
     ArrayList<Pregunta> crearPreguntas() {
         ArrayList<Pregunta> preguntas = new ArrayList<>();
@@ -101,11 +95,13 @@ public class Preguntas extends AppCompatActivity {
         opcion_3 = (Button) findViewById(R.id.btn_op_3);
         opcion_4 = (Button) findViewById(R.id.btn_op_4);
         //preguntas_expuestas = new ArrayList<>();
-        Ganador = false;
-        respuesta_correcta = true;
-        int aciertos = 0;
-        ArrayList<Pregunta> p = crearPreguntas();
+        //Ganador = false;
+        //respuesta_correcta = true;
+        aciertos = 0;
+        p = crearPreguntas();
         random = crearRandom();
+        mostrarPreguntas(p,random.get(aciertos));
+
     }
 
     void mostrarPreguntas(ArrayList<Pregunta> p,int n){
@@ -114,14 +110,14 @@ public class Preguntas extends AppCompatActivity {
             opcion_2.setText(p.get(n).getResp().get(1));
             opcion_3.setText(p.get(n).getResp().get(2));
             opcion_4.setText(p.get(n).getResp().get(3));
-            //preguntas_expuestas.add(n);
+
     }
 
     ArrayList<Integer> crearRandom(){
         ArrayList<Integer> lista = new ArrayList<>();
         while(lista.size()<5){
             Random rand = new Random();
-            Integer n = rand.nextInt(4)+1;
+            Integer n = rand.nextInt(5);
             if (!lista.contains(n)){
                 lista.add(n);
             }
@@ -130,18 +126,81 @@ public class Preguntas extends AppCompatActivity {
     }
 
     public void onClick(View view){
-
-        if(opcion_1.getText().equals(p.get(aciertos).getRespCorr()) && opcion_1.isPressed()){
-            aciertos+=1;
-            if(aciertos==5){
-                Intent victoria = new Intent(Preguntas.this, P_Acierto.class);
-                startActivity(victoria);
+        System.out.println(random.toString());
+        if(opcion_1.isPressed()) {
+            if (opcion_1.getText().equals(p.get(random.get(aciertos)).getRespCorr())) {
+                aciertos += 1;
+                if (aciertos == 5) {
+                    aciertos=0;
+                    Intent victoria = new Intent(Preguntas.this, P_Acierto.class);
+                    startActivity(victoria);
+                }
+                mostrarPreguntas(p, random.get(aciertos));
+            }else{
+                Intent fallo = new Intent(Preguntas.this, P_Fallo.class);
+                Bundle bund = new Bundle();
+                bund.putSerializable("aciertos",aciertos);
+                fallo.putExtras(bund);
+                startActivity(fallo);
+                aciertos=0;
             }
-            mostrarPreguntas(p,random.get(aciertos));
-        }else{
-            Intent fallo = new Intent(Preguntas.this, P_Fallo.class);
-            startActivity(fallo);
         }
+        else if(opcion_2.isPressed()){
+            if (opcion_2.getText().equals(p.get(random.get(aciertos)).getRespCorr())) {
+                aciertos += 1;
+                if (aciertos == 5) {
+                    aciertos=0;
+                    Intent victoria = new Intent(Preguntas.this, P_Acierto.class);
+                    startActivity(victoria);
+                }
+                mostrarPreguntas(p, random.get(aciertos));
+            }else{
+                Intent fallo = new Intent(Preguntas.this, P_Fallo.class);
+                Bundle bund = new Bundle();
+                bund.putSerializable("aciertos",aciertos);
+                fallo.putExtras(bund);
+                startActivity(fallo);
+                aciertos=0;
+            }
+        }
+        else if(opcion_3.isPressed()){
+            if (opcion_3.getText().equals(p.get(random.get(aciertos)).getRespCorr())) {
+                aciertos += 1;
+                if (aciertos == 5) {
+                    aciertos=0;
+                    Intent victoria = new Intent(Preguntas.this, P_Acierto.class);
+                    startActivity(victoria);
+                }
+                mostrarPreguntas(p, random.get(aciertos));
+            }else{
+                Intent fallo = new Intent(Preguntas.this, P_Fallo.class);
+                Bundle bund = new Bundle();
+                bund.putSerializable("aciertos",aciertos);
+                fallo.putExtras(bund);
+                startActivity(fallo);
+                aciertos=0;
+            }
+        }
+        else if(opcion_4.isPressed()){
+            if (opcion_4.getText().equals(p.get(random.get(aciertos)).getRespCorr())) {
+                aciertos += 1;
+                if (aciertos == 5) {
+                    aciertos=0;
+                    Intent victoria = new Intent(Preguntas.this, P_Acierto.class);
+                    startActivity(victoria);
+                }
+                mostrarPreguntas(p, random.get(aciertos));
+            }else{
+                Intent fallo = new Intent(Preguntas.this, P_Fallo.class);
+                Bundle bund = new Bundle();
+                bund.putSerializable("aciertos",aciertos);
+                fallo.putExtras(bund);
+                startActivity(fallo);
+                aciertos=0;
+            }
+        }
+
+
     }
 
 
